0a640d703712bcf98817997371491d6c2bb887a6
AngelsGV <angels.not99@gmail.com>
2024-03-11 19:00:58 +0100

    Initial commit

--- src/Criba.java
+++ src/Criba.java
@@ -1,11 +1,4 @@
 import java.util.Scanner;
-
-/**
- * @version 20.0
- * @author  Angels
- * @since 11/3/24
- * primera refaroctirazocion
- */
 public class Criba
 {
     // Generar números primos de 1 a max
@@ -17,7 +10,8 @@ public class Criba
             int dim = max + 1; // Tamaño del array
             boolean[] esPrimo = new boolean[dim];
 // Inicializar el array
-            iniciaVector(dim, esPrimo);
+            for (i=0; i<dim; i++)
+                esPrimo[i] = true;
 // Eliminar el 0 y el 1, que no son primos
             esPrimo[0] = esPrimo[1] = false;
 // Criba
@@ -35,54 +29,32 @@ public class Criba
                     cuenta++;
             }
 // Rellenar el vector de números primos
-            int[] primos = rellenaVector(cuenta, dim, esPrimo);
-            return primos;
-} else { // max < 2
-        return new int[0];
-// Vector vacío
-        }
-        }
-
-    /**
-     *
-     * @param cuenta explicar
-
-     * @param dim dimension del vector
-     * @param esPrimo vectr de booleano
-     * @return
-     */
-    public static int[] rellenaVector(int cuenta, int dim, boolean[] esPrimo) {
-        int j;
-        int i;
-        int[] primos = new int[cuenta];
-        for (i=0, j=0; i< dim; i++) {
+            int[] primos = new int[cuenta];
+            for (i=0, j=0; i<dim; i++) {
                 if (esPrimo[i])
                     primos[j++] = i;
             }
-        return primos;
+            return primos;
+        } else { // max < 2
+            return new int[0];
+// Vector vacío
+        }
     }
-
-    public static void iniciaVector(int dim, boolean[] esPrimo) {
-        int i;
-        for (i=0; i< dim; i++)
-            esPrimo[i] = true;
-    }
-
     public static void main(String[] args) {
         Scanner teclado=new Scanner(System.in);
         System.out.println("Introduce el número para la criba de Erastótenes:");
-        int dato = teclado.nextInt();
+        int dato=teclado.nextInt();
         int vector[]=new int[dato];
         System.out.println("\nVector inicial hasta :"+dato);
         for (int i = 0; i < vector.length; i++) {
-        if (i%10==0) System.out.println();
-        System.out.print(i+1+"\t");
+            if (i%10==0) System.out.println();
+            System.out.print(i+1+"\t");
         }
         vector=generarPrimos(dato);
         System.out.println("\nVector de primos hasta:"+dato);
         for (int i = 0; i < vector.length; i++) {
-        if (i%10==0) System.out.println();
-        System.out.print(vector[i]+"\t");
+            if (i%10==0) System.out.println();
+            System.out.print(vector[i]+"\t");
         }
-        }
-        }
+    }
+}
